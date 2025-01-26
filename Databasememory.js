9a909e2307dd2f68a4cb47d0fe7ac97c3376ee5b
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-01-25 22:50:11 -0400

    Inicio 25/01 problemas com o numero de servidor

--- Databasememory.js
+++ Databasememory.js
@@ -1,3 +1,32 @@
+ import { randomUUID } from "node:crypto"
+ 
  export class Databasememory{
+  #sapatos = new Map()
 
+     // Lista todos os sapatos com possibilidade de pesquisa
+  list(search) {
+    return Array.from(this.#sapatos.entries())
+      .map(([id, data]) => ({
+        id,
+        ...data,
+      }))
+      .filter((tenis) => {
+        if (search) {
+          return tenis.nome && tenis.nome.toLowerCase().includes(search.toLowerCase());
+        }
+        return true; // Retorna todos os tênis caso não haja filtro
+      });
+  }
+  
+  create(tenis){
+    const tenisId = randomUUID()
+    this.#sapatos.set(tenisId, tenis)
+  }
+
+  update(id, tenis){
+    this.#sapatos.set(id, tenis)
+  }
+  delete(id){
+    this.#sapatos.delete(id)
+  }
 }
--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 import { fastify } from 'fastify';
 import { Databasememory } from './Databasememory.js'
 const server = fastify();
+const database = new Databasememory()
 
 // Metodos de POST, GET, PUT, PATCH e DELETE
+server.post('/sapatos', (request , reply) =>{
+    const {tamanho, id, modelo, nome, cor, preco} = request.body
+
+    database.create({
+        tamanho: tamanho,
+        id: id,
+        modelo: modelo,
+        nome: nome,
+        cor: cor,
+        preco: preco
+    })
+   
+    return reply.status(201).send()
+});
+
 server.get('/sapatos', () =>{
-
-})
-
-server.post('/sapatos', () =>{
-
-})
+ return 'seu pai é gay'
+});
 
 server.put('/sapatos', () =>{
 
-})
+});
 
 server.patch('/sapatos', () =>{
 
-})
+});
 
 server.delete('/sapatos', () =>{
 
-})
+});
+
+server.listen({
+  port: 3333,
+
+});
+    
