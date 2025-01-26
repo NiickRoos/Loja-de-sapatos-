6f5ba1184a4285a6665c061472110f8a0aa0cfaa
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-01-25 22:49:21 -0400

    Inicio do projeto

---
+++ server.js
@@ -0,0 +1,24 @@
+import { fastify } from 'fastify';
+import { Databasememory } from './Databasememory.js'
+const server = fastify();
+
+// Metodos de POST, GET, PUT, PATCH e DELETE
+server.get('/sapatos', () =>{
+
+})
+
+server.post('/sapatos', () =>{
+
+})
+
+server.put('/sapatos', () =>{
+
+})
+
+server.patch('/sapatos', () =>{
+
+})
+
+server.delete('/sapatos', () =>{
+
+})
