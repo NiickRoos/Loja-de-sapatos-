f3b91bddc1e63078082f5d1875287c55db6c98c8
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-01-26 10:13:26 -0400

    Organização de algumas coisas do server  e retirada de coisas que não eram necessarias

--- Databasememory.js
+++ Databasememory.js
@@ -1,5 +1,5 @@
  import { randomUUID } from "node:crypto"
- 
+
  export class Databasememory{
   #sapatos = new Map()
 
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify';
 import { Databasememory } from './Databasememory.js'
-import { request } from 'node:http';
+
 const server = fastify();
 const database = new Databasememory()
 
 // Metodos de POST, GET, PUT, PATCH e DELETE
+
 server.post('/sapatos', (request , reply) =>{
+  
     const {tamanho,  modelo, nome, cor, preco} = request.body
 
     database.create({
@@ -15,20 +17,28 @@ server.post('/sapatos', (request , reply) =>{
         cor: cor,
         preco: preco
     })
+
     console.log(database.list())
     return reply.status(201).send()
+
 });
 
+
 server.get('/sapatos', (request) =>{
+
    const search = request.query.search
    const sapatos = database.list(search)
    console.log(sapatos)
    return sapatos
+
 });
 
+
 server.put('/sapatos/:id', (request, reply) =>{
+
   const tenisId = request.params.id
   const {tamanho,  modelo, nome, cor, preco} = request.body
+
   database.update(tenisId, {
    tamanho,
    modelo,
@@ -36,15 +46,19 @@ server.put('/sapatos/:id', (request, reply) =>{
    cor,
    preco
   })
+
   return reply.status(204).send
+
 });
 
 //Atualizar os recursos
 server.patch('/sapatos/:id', (request, reply) =>{
+
   const tenisId = request.params.id;
   const Dadosatualizados = request.body;
   const sapatos = database.list();
   const  sapato = sapatos.find((tenis) => tenis.id = tenisId);
+
   if (!sapato) { // Verifica se o tênis existe.
     return reply.status(404).send({ error: 'Tênis não encontrado' }); // Retorna um erro 404 se o tênis não for encontrado.
   }
@@ -60,15 +74,19 @@ server.patch('/sapatos/:id', (request, reply) =>{
 });
 
 server.delete('/sapatos/:id', (request, reply) =>{
+
  const tenisId = request.params.id
  database.delete(tenisId)
  return reply.status(204).send()
+
 });
 
 server.listen({ port: 3333 }, (err, address) => {
+
     if (err) {
       console.error(err);
       process.exit(1);
     }
     console.log(`Servidor rodando em ${address}`);
+    
   });
