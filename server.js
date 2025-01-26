5b0d168cd5e4eb18a83b0d6d13ab622619005cb0
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-01-26 00:17:16 -0400

    26/01 arrumando erros e criação do delete entre outras

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
 import { fastify } from 'fastify';
 import { Databasememory } from './Databasememory.js'
+import { request } from 'node:http';
 const server = fastify();
 const database = new Databasememory()
 
 // Metodos de POST, GET, PUT, PATCH e DELETE
 server.post('/sapatos', (request , reply) =>{
-    const {tamanho, id, modelo, nome, cor, preco} = request.body
+    const {tamanho,  modelo, nome, cor, preco} = request.body
 
     database.create({
         tamanho: tamanho,
-        id: id,
         modelo: modelo,
         nome: nome,
         cor: cor,
         preco: preco
     })
-   
+    console.log(database.list())
     return reply.status(201).send()
 });
 
 server.get('/sapatos', () =>{
- return ' funcionadoo'
+   const sapatos = database.list()
+   console.log(sapatos)
+   return sapatos
 });
 
-server.put('/sapatos', () =>{
-
+server.put('/sapatos/:id', (request, reply) =>{
+  const tenisId = request.params.id
+  const {tamanho,  modelo, nome, cor, preco} = request.body
+  database.update(tenisId, {
+   tamanho,
+   modelo,
+   nome,
+   cor,
+   preco
+  })
+  return reply.status(204).send
 });
 
 server.patch('/sapatos', () =>{
 
 });
 
-server.delete('/sapatos', () =>{
-
+server.delete('/sapatos/:id', (request, reply) =>{
+ const tenisId = request.params.id
+ database.delete(tenisId)
+ return reply.status(204).send()
 });
 
 server.listen({ port: 3333 }, (err, address) => {
