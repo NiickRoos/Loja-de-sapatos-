27554ae957a3f3cca8bd127a7ac61591d014c322
NiickRoos <nicole.roos@estudante.ifms.edu.br>
2025-01-26 10:08:57 -0400

    Implementação do patch

--- server.js
+++ server.js
@@ -39,7 +39,23 @@ server.put('/sapatos/:id', (request, reply) =>{
   return reply.status(204).send
 });
 
-server.patch('/sapatos', () =>{
+//Atualizar os recursos
+server.patch('/sapatos/:id', (request, reply) =>{
+  const tenisId = request.params.id;
+  const Dadosatualizados = request.body;
+  const sapatos = database.list();
+  const  sapato = sapatos.find((tenis) => tenis.id = tenisId);
+  if (!sapato) { // Verifica se o tênis existe.
+    return reply.status(404).send({ error: 'Tênis não encontrado' }); // Retorna um erro 404 se o tênis não for encontrado.
+  }
+
+  const sapatoAtualizado = { ...sapato, ...Dadosatualizados }; 
+  // Atualiza apenas os atributos enviados. Os atributos ausentes permanecem iguais ao original.
+
+  database.update(tenisId, sapatoAtualizado); 
+  // Substitui o objeto original pelo objeto atualizado no banco de dados em memória.
+
+  return reply.status(204).send(); 
 
 });
 
